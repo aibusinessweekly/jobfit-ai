b79c2e8e36011fb9915f49db284170d64a38fa37
aibusinessweekly <partnerships@aibusinessweekly.ca>
2026-03-05 13:36:04 -0500

    Update search.js

--- api/search.js
+++ api/search.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { query, location } = req.query;
-  const url = `https://jsearch.p.rapidapi.com/search?query=${encodeURIComponent(query+' '+location)}&page=1&num_pages=2&date_posted=month`;
+  const url = `https://jsearch.p.rapidapi.com/search?query=${encodeURIComponent(query+' '+location)}&page=1&num_pages=3&remote_jobs_only=false`;
 
   const response = await fetch(url, {
     headers: {
